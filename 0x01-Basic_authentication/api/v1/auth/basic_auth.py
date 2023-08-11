a5210831287fadcbafff4e24aec0a7ef78be7fc6
Gachenge <gachenge1@gmail.com>
2023-08-11 08:24:28 +0000

    basic authentication class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-
+"""class that provides basic authentication
+"""
 from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
+    """provides basic authentivation to API"""
     pass
