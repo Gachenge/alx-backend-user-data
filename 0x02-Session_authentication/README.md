833bf212570b52b678027f15e69edf96d6460284
Gachenge <gachenge1@gmail.com>
2023-08-12 08:54:12 +0300

    session authentication

---
+++ 0x02-Session_authentication/README.md
@@ -0,0 +1,5 @@
+session authentication
+REST API Authentication Mechanisms - Only the session auth part
+HTTP Cookie
+Flask
+Flask Cookie
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" class that handles API authentication """
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ to handle API authentication """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ check authentication"""
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        for excluded in excluded_paths:
+            if excluded.startswith(path):
+                return False
+            elif path.startswith(excluded):
+                return False
+            elif excluded[-1] == '*':
+                if path.startswith(excluded[:-1]):
+                    return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """return authorisation header from request"""
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """return current user from request"""
+        return None
